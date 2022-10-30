a63fff9cedce71b8dd3beb82a21abb4ab3d134cf
Coder Suresh <sd807141@gmail.com>
2022-10-30 15:21:35 +0545

    fixed image not showing issue

--- profile-card/src/components/Footer.jsx
+++ profile-card/src/components/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import twitterIcon from '../../images/icon_twitter.svg'
+import facebookIcon from '../../images/icon_facebook.svg'
+import instagramIcon from '../../images/icon_instagram.svg'
+import githubIcon from '../../images/icon_github.svg'
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <img src='../../images/icon_twitter.svg' alt='twitter' aria-label='twitter'/>
-      <img src='../../images/icon_facebook.svg' alt='facebook' aria-label='facebook'/>
-      <img src='../../images/icon_instagram.svg' alt='instagram' aria-label='instagram'/>
-      <img src='../../images/icon_github.svg' alt='github' aria-label='github'/>
+      <img src={twitterIcon} alt='twitter' aria-label='twitter'/>
+      <img src={facebookIcon} alt='facebook' aria-label='facebook'/>
+      <img src={instagramIcon} alt='instagram' aria-label='instagram'/>
+      <img src={githubIcon} alt='github' aria-label='github'/>
     </footer>
   )
 }
